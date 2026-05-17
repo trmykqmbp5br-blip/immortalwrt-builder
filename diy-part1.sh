f595e9f49a589afe2ab6944b38dc999c8f577bf3
trmykqmbp5br-blip <trmykqmbp5br-blip@users.noreply.github.com>
2026-05-17 23:25:22 +0800

    fix: 启用 sirpdboy feed（luci-app-argon-config 来源）

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,8 +8,8 @@ echo "src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master" >> f
 # small: 依赖包 (xray-core, sing-box, hysteria 等)
 echo "src-git small https://github.com/kenzok8/small.git;master" >> feeds.conf.default
 
-# sirpdboy: 高级设置、分区扩容、酷猫主题等
-# echo "src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package.git;main" >> feeds.conf.default
+# sirpdboy: 高级设置、分区扩容、酷猫主题、luci-app-argon-config 等
+echo "src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package.git;main" >> feeds.conf.default
 
 # animegasan: luci-app-alpha-config (luci-theme-alpha 依赖)
 echo "src-git animegasan https://github.com/animegasan/luci-app-alpha-config.git;master" >> feeds.conf.default
